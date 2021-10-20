35d986a328658704defca0a22611244133a14f0f
Fredrik Jonsén <frjonsen@users.noreply.github.com>
2021-10-20 22:12:08 +0200

    basics of getting document names

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,24 @@
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 interface AppState {
   pdfs: Array<String>
+  currentDocumentTotalPages: number
 }
 
 const Pdf = (props: { pdfs: Array<String> }) => <ul id="pdfList">{props.pdfs.map(f => <li>{f}</li>)}</ul>;
 
 class App extends React.Component<{}, AppState> {
   state: AppState = {
-    pdfs: ["1", "2", "3"]
+    pdfs: ["1", "2", "3"],
+    currentDocumentTotalPages: 0
   };
 
   doDifferentState = () => {
     this.setState({
-      pdfs: ["2", "3", "4"]
+      pdfs: ["2", "3", "4"],
     });
   }
 
@@ -25,10 +28,24 @@ class App extends React.Component<{}, AppState> {
     this.setState({ pdfs: documents });
   }
 
+  onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    this.setState({
+      currentDocumentTotalPages: numPages
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Pdf pdfs={this.state.pdfs} />
+        <div id="documentViewer">
+
+          <Document file="/api/documents/1" onLoadSuccess={this.onDocumentLoadSuccess}>
+            <Page pageNumber={1} />
+            <Page pageNumber={2} />
+          </Document>
+        </div>
+        <p>{this.state.currentDocumentTotalPages} pages in this document</p>
       </div>
     );
   }
